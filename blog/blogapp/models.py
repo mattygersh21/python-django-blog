cf01bc5dff573533b75a4af259069542ff421160
Matthew Gerszewski <dakotaseia@hotmail.com>
2022-10-09 13:40:02 -0500

    * Created a custom model manager called PublishedManager.

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+  def get_queryset(self):
+    return super().get_queryset()\
+      .filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
   class Status(models.TextChoices):
     DRAFT = 'DF', 'Draft'
@@ -15,6 +20,8 @@ class Post(models.Model):
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
   status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+  objects = models.Manager()
+  published = PublishedManager()
 
   class Meta:
     ordering = ['-publish']
